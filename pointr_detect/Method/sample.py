c95c6386fe0b8830d84d4cfc0ce8d9263bd2ec1c
chLi <565353780@qq.com>
2022-11-19 00:30:14 +0800

    add sample algo

---
+++ pointr_detect/Method/sample.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import torch
+import random
+from pointnet2_ops import pointnet2_utils
+
+
+def fps(data, number):
+    '''
+        data B N 3
+        number int
+    '''
+    fps_idx = pointnet2_utils.furthest_point_sample(data, number)
+    fps_data = pointnet2_utils.gather_operation(
+        data.transpose(1, 2).contiguous(), fps_idx).transpose(1,
+                                                              2).contiguous()
+    return fps_data
+
+
+def seprate_point_cloud(xyz,
+                        num_points,
+                        crop,
+                        fixed_points=None,
+                        padding_zeros=False):
+    '''
+     seprate point cloud: usage : using to generate the incomplete point cloud with a setted number.
+    '''
+    _, n, c = xyz.shape
+
+    assert n == num_points
+    assert c == 3
+    if crop == num_points:
+        return xyz, None
+
+    INPUT = []
+    CROP = []
+    for points in xyz:
+        if isinstance(crop, list):
+            num_crop = random.randint(crop[0], crop[1])
+        else:
+            num_crop = crop
+
+        points = points.unsqueeze(0)
+
+        if fixed_points is None:
+            center = F.normalize(torch.randn(1, 1, 3), p=2, dim=-1).cuda()
+        else:
+            if isinstance(fixed_points, list):
+                fixed_point = random.sample(fixed_points, 1)[0]
+            else:
+                fixed_point = fixed_points
+            center = fixed_point.reshape(1, 1, 3).cuda()
+
+        distance_matrix = torch.norm(center.unsqueeze(2) - points.unsqueeze(1),
+                                     p=2,
+                                     dim=-1)  # 1 1 2048
+
+        idx = torch.argsort(distance_matrix, dim=-1,
+                            descending=False)[0, 0]  # 2048
+
+        if padding_zeros:
+            input_data = points.clone()
+            input_data[0, idx[:num_crop]] = input_data[0, idx[:num_crop]] * 0
+
+        else:
+            input_data = points.clone()[0,
+                                        idx[num_crop:]].unsqueeze(0)  # 1 N 3
+
+        crop_data = points.clone()[0, idx[:num_crop]].unsqueeze(0)
+
+        if isinstance(crop, list):
+            INPUT.append(fps(input_data, 2048))
+            CROP.append(fps(crop_data, 2048))
+        else:
+            INPUT.append(input_data)
+            CROP.append(crop_data)
+
+    input_data = torch.cat(INPUT, dim=0)  # B N 3
+    crop_data = torch.cat(CROP, dim=0)  # B M 3
+
+    return input_data.contiguous(), crop_data.contiguous()
