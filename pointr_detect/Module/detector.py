0ad59b7e6611f6bf069e9512892540006bd1c615
chLi <565353780@qq.com>
2022-11-19 00:30:25 +0800

    finish detect with dataloader

--- pointr_detect/Module/detector.py
+++ pointr_detect/Module/detector.py
@@ -3,14 +3,35 @@
 
 import os
 import torch
+import numpy as np
+import open3d as o3d
+from tqdm import tqdm
+
 from pointr_detect.Model.pointr import PoinTr
 
 from pointr_detect.Data.average_meter import AverageMeter
 
 from pointr_detect.Dataset.shapenet_55 import ShapeNet55Dataset
 
+from pointr_detect.Method.sample import fps, seprate_point_cloud
+from pointr_detect.Method.move import moveToOrigin, moveToMeanPoint
+from pointr_detect.Method.render import renderPointArrayWithUnitBBox
+
+choice = [
+    torch.Tensor([1, 1, 1]),
+    torch.Tensor([1, 1, -1]),
+    torch.Tensor([1, -1, 1]),
+    torch.Tensor([-1, 1, 1]),
+    torch.Tensor([-1, -1, 1]),
+    torch.Tensor([-1, 1, -1]),
+    torch.Tensor([1, -1, -1]),
+    torch.Tensor([-1, -1, -1])
+]
+
+
 def worker_init_fn(worker_id):
     np.random.seed(np.random.get_state()[1][0] + worker_id)
+
 
 class Detector(object):
 
@@ -51,11 +72,28 @@ class Detector(object):
         data = {'inputs': {}, 'predictions': {}, 'losses': {}, 'logs': {}}
 
         dataset = ShapeNet55Dataset()
-        dataloader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=1,
-            shuffle=False,
-            drop_last=False,
-            num_workers=4,
-            worker_init_fn=worker_init_fn)
+        dataloader = torch.utils.data.DataLoader(dataset,
+                                                 batch_size=1,
+                                                 shuffle=False,
+                                                 drop_last=False,
+                                                 num_workers=4,
+                                                 worker_init_fn=worker_init_fn)
+        for _, _, data in tqdm(dataloader):
+            gt = data.cuda()
+            num_crop = int(8192 / 2)
+            for item in choice:
+                renderPointArrayWithUnitBBox(data[0])
+                partial, _ = seprate_point_cloud(gt,
+                                                 8192,
+                                                 num_crop,
+                                                 fixed_points=item)
+                partial = fps(partial, 2048)
+
+                #  points = partial.cpu().numpy()[0]
+                #  points = moveToMeanPoint(points).reshape(1, -1, 3)
+                #  partial = torch.tensor(points).cuda()
+
+                renderPointArrayWithUnitBBox(partial[0])
+                coarse_points, dense_points = self.model(partial)
+                renderPointArrayWithUnitBBox(dense_points[0])
         return data
