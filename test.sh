a53069882031a1042b83e5d4aa4e750c0322ecc7
chLi <565353780@qq.com>
2022-11-20 19:23:03 +0800

    remove old running bash file

--- test.sh
+++
@@ -1,8 +0,0 @@
-cd ../PoinTr
-
-bash ./scripts/test.sh 0 \
-    --ckpts /home/chli/chLi/PoinTr/pointr_training_from_scratch_c55_best.pth \
-    --config ./cfgs/ShapeNet55_models/PoinTr.yaml \
-    --mode easy \
-    --exp_name example
-
