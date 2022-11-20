5d7fe8e1082ba855f3aae48e8b86aeb2ef01ea96
chLi <565353780@qq.com>
2022-11-20 19:21:48 +0800

    add 5 different losses

---
+++ pointr_detect/Loss/ious.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pointr_detect.Loss.iou.iou import IoU
+from pointr_detect.Loss.iou.ciou import CIoU
+from pointr_detect.Loss.iou.diou import DIoU
+from pointr_detect.Loss.iou.eiou import EIoU
+from pointr_detect.Loss.iou.giou import GIoU
