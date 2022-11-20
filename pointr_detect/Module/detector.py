b330618206db9a225f0fcb5618bdcf54a49c02fc
chLi <565353780@qq.com>
2022-11-20 19:22:22 +0800

    remove unused old model

--- pointr_detect/Module/detector.py
+++ pointr_detect/Module/detector.py
@@ -9,10 +9,8 @@ from tqdm import tqdm
 
 from pointr_detect.Model.pointr import PoinTr
 
-from pointr_detect.Method.sample import fps, seprate_point_cloud
-from pointr_detect.Method.trans import moveToOrigin, moveToMeanPoint
+from pointr_detect.Method.sample import fps
 from pointr_detect.Method.device import toCuda
-from pointr_detect.Method.render import renderPointArrayWithUnitBBox
 
 
 class Detector(object):
@@ -23,27 +21,6 @@ class Detector(object):
         if model_file_path is not None:
             self.loadModel(model_file_path)
         return
-
-    def loadPoinTrModel(self, model_file_path):
-        assert os.path.exists(model_file_path)
-
-        print("[INFO][Detector::loadModel]")
-        print("\t start loading pointr model from :")
-        print("\t", model_file_path)
-
-        state_dict = torch.load(model_file_path)
-        if state_dict.get('model') is not None:
-            base_ckpt = {
-                k.replace("module.", ""): v
-                for k, v in state_dict['model'].items()
-            }
-        elif state_dict.get('base_model') is not None:
-            base_ckpt = {
-                k.replace("module.", ""): v
-                for k, v in state_dict['base_model'].items()
-            }
-        self.model.load_state_dict(base_ckpt)
-        return True
 
     def loadModel(self, model_file_path):
         assert os.path.exists(model_file_path)
