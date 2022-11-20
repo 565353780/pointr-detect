1b2ce1006c6424997d2826e8e66458f259b37399
chLi <565353780@qq.com>
2022-11-20 22:49:03 +0800

    rebuild model struct, split into several functions

--- points_shape_detect/Model/points_shape_net.py
+++ points_shape_detect/Model/points_shape_net.py
@@ -6,11 +6,10 @@ from pointnet2_ops import pointnet2_utils
 from torch import nn
 
 from points_shape_detect.Lib.chamfer_dist import ChamferDistanceL1
+from points_shape_detect.Loss.ious import IoULoss
 from points_shape_detect.Method.sample import fps
 from points_shape_detect.Model.fold import Fold
 from points_shape_detect.Model.pc_transformer import PCTransformer
-
-from points_shape_detect.Loss.ious import IoULoss
 
 
 class PointsShapeNet(nn.Module):
@@ -24,12 +23,12 @@ class PointsShapeNet(nn.Module):
 
         self.fold_step = int(pow(self.num_pred // self.num_query, 0.5) + 0.5)
 
-        self.base_model = PCTransformer(in_chans=3,
-                                        embed_dim=self.trans_dim,
-                                        depth=[6, 8],
-                                        drop_rate=0.,
-                                        num_query=self.num_query,
-                                        knn_layer=self.knn_layer)
+        self.feature_encoder = PCTransformer(in_chans=3,
+                                             embed_dim=self.trans_dim,
+                                             depth=[6, 8],
+                                             drop_rate=0.,
+                                             num_query=self.num_query,
+                                             knn_layer=self.knn_layer)
 
         self.foldingnet = Fold(self.trans_dim,
                                step=self.fold_step,
@@ -59,38 +58,29 @@ class PointsShapeNet(nn.Module):
         self.l1_loss = nn.SmoothL1Loss()
         return
 
-    def get_loss(self, data):
-        loss_coarse = self.loss_func(data['predictions']['coarse_points'],
-                                     data['inputs']['point_array'])
-        loss_fine = self.loss_func(data['predictions']['dense_points'],
-                                   data['inputs']['point_array'])
+    def encodePoints(self, data):
+        # Bx#pointx3
+        sample_point_array = data['inputs']['sample_point_array']
 
-        loss_bbox_l1 = self.l1_loss(data['predictions']['bbox'],
-                                    data['inputs']['bbox'])
-        loss_center_l1 = self.l1_loss(data['predictions']['center'],
-                                      data['inputs']['center'])
+        # Bx#pointx3 -[feature_encoder]-> BxMxC and BxMx3
+        encode_feature, coarse_point_cloud = self.feature_encoder(
+            sample_point_array)
 
-        loss_bbox_eiou = torch.mean(
-            IoULoss.EIoU(data['predictions']['bbox'], data['inputs']['bbox']))
-
-        data['losses']['loss_coarse'] = loss_coarse * 1000
-        data['losses']['loss_fine'] = loss_fine * 1000
-        data['losses']['loss_bbox_l1'] = loss_bbox_l1 * 1000
-        data['losses']['loss_center_l1'] = loss_center_l1 * 1000
-        data['losses']['loss_bbox_eiou'] = loss_bbox_eiou
+        data['predictions']['encode_feature'] = encode_feature
+        data['predictions']['coarse_point_cloud'] = coarse_point_cloud
         return data
 
-    def forward(self, data):
-        # Bx#pointx3
-        point_array = data['inputs']['point_array']
+    def decodePointsFeature(self, data):
+        # BxMxC
+        encode_feature = data['predictions']['encode_feature']
+        # BxMx3
+        coarse_point_cloud = data['predictions']['coarse_point_cloud']
 
-        # Bx#pointx3 -[base_model]-> BxMxC and BxMx3
-        q, coarse_point_cloud = self.base_model(point_array)
-
-        B, M, C = q.shape
+        B, M, C = data['predictions']['encode_feature'].shape
 
         # BxMxC -[transpose]-> BxCxM -[increase_dim]-> Bx1024xM -[transpose]-> BxMx1024
-        points_feature = self.increase_dim(q.transpose(1, 2)).transpose(1, 2)
+        points_feature = self.increase_dim(encode_feature.transpose(
+            1, 2)).transpose(1, 2)
 
         # BxMx1024 -[max]-> Bx1024
         global_points_feature = torch.max(points_feature, dim=1)[0]
@@ -100,12 +90,38 @@ class PointsShapeNet(nn.Module):
             -2).expand(-1, M, -1)
 
         # BxMx1024 + BxMxC + BxMx3 -[cat]-> BxMx(C+1027)
-        global_feature = torch.cat(
-            [replicate_global_points_feature, q, coarse_point_cloud], dim=-1)
+        global_feature = torch.cat([
+            replicate_global_points_feature, encode_feature, coarse_point_cloud
+        ],
+                                   dim=-1)
 
         # BxMx(C+1027) -[reshape]-> BMx(C+1027) -[reduce_map]-> BMxC
         reduce_global_feature = self.reduce_map(
             global_feature.reshape(B * M, -1))
+
+        data['predictions']['reduce_global_feature'] = reduce_global_feature
+        return data
+
+    def lossBBox(self, data):
+        bbox = data['predictions']['bbox']
+        center = data['predictions']['center']
+        gt_bbox = data['inputs']['bbox']
+        gt_center = data['inputs']['center']
+
+        loss_bbox_l1 = self.l1_loss(bbox, gt_bbox)
+        loss_center_l1 = self.l1_loss(center, gt_center)
+        loss_bbox_eiou = torch.mean(IoULoss.EIoU(bbox, gt_bbox))
+
+        data['losses']['loss_bbox_l1'] = loss_bbox_l1 * 1000
+        data['losses']['loss_center_l1'] = loss_center_l1 * 1000
+        data['losses']['loss_bbox_eiou'] = loss_bbox_eiou
+        return data
+
+    def encodeBBox(self, data):
+        # BMxC
+        reduce_global_feature = data['predictions']['reduce_global_feature']
+
+        B, M, C = data['predictions']['encode_feature'].shape
 
         # BMxC -[reshape]-> BxMCx1 -[bbox_feature_decoder]-> BxCx1
         bbox_feature = self.bbox_feature_decoder(
@@ -116,6 +132,21 @@ class PointsShapeNet(nn.Module):
 
         # BxCx1 -[center_decoder]-> Bx3x1 -[reshape]-> Bx3
         center = self.center_decoder(bbox_feature).reshape(B, -1)
+
+        data['predictions']['bbox'] = bbox
+        data['predictions']['center'] = center
+
+        if self.training:
+            data = self.lossBBox(data)
+        return data
+
+    def decodePatchPoints(self, data):
+        # BMxC
+        reduce_global_feature = data['predictions']['reduce_global_feature']
+        # BxMx3
+        coarse_point_cloud = data['predictions']['coarse_point_cloud']
+
+        B, M, C = data['predictions']['encode_feature'].shape
 
         # BMxC -[foldingnet]-> BMx3xS -[reshape]-> BxMx3xS
         relative_patch_points = self.foldingnet(reduce_global_feature).reshape(
@@ -129,25 +160,54 @@ class PointsShapeNet(nn.Module):
         # BxMxSx3 -[reshape]-> BxMSx3
         rebuild_points = rebuild_patch_points.reshape(B, -1, 3)
 
+        data['predictions']['rebuild_patch_points'] = rebuild_patch_points
+        data['predictions']['rebuild_points'] = rebuild_points
+        return data
+
+    def get_loss(self, data):
+        loss_coarse = self.loss_func(data['predictions']['coarse_points'],
+                                     data['inputs']['point_array'])
+        loss_fine = self.loss_func(data['predictions']['dense_points'],
+                                   data['inputs']['point_array'])
+
+        data['losses']['loss_coarse'] = loss_coarse * 1000
+        data['losses']['loss_fine'] = loss_fine * 1000
+        return data
+
+    def embedPoints(self, data):
+        # Bx#pointx3
+        sample_point_array = data['inputs']['sample_point_array']
+        # BxMx3
+        coarse_point_cloud = data['predictions']['coarse_point_cloud']
+        # BxMSx3
+        rebuild_points = data['predictions']['rebuild_points']
+
         # Bx#pointx3 -[fps]-> BxMx3
-        sample_point_array = fps(point_array, self.num_query)
+        fps_sample_point_array = fps(sample_point_array, self.num_query)
 
         # BxMx3 + BxMx3 -[cat]-> Bx2Mx3
-        coarse_points = torch.cat([coarse_point_cloud, sample_point_array],
+        coarse_points = torch.cat([coarse_point_cloud, fps_sample_point_array],
                                   dim=1).contiguous()
 
         # BxMSx3 + Bx#pointx3 -[cat]-> Bx(MS+#point)x3
-        dense_points = torch.cat([rebuild_points, point_array],
+        dense_points = torch.cat([rebuild_points, sample_point_array],
                                  dim=1).contiguous()
 
-        data['predictions']['coarse_point_cloud'] = coarse_point_cloud
-        data['predictions']['rebuild_patch_points'] = rebuild_patch_points
-        data['predictions']['rebuild_points'] = rebuild_points
         data['predictions']['coarse_points'] = coarse_points
         data['predictions']['dense_points'] = dense_points
-        data['predictions']['bbox'] = bbox
-        data['predictions']['center'] = center
 
         if self.training:
             data = self.get_loss(data)
+        return data
+
+    def forward(self, data):
+        data = self.encodePoints(data)
+
+        data = self.decodePointsFeature(data)
+
+        data = self.encodeBBox(data)
+
+        data = self.decodePatchPoints(data)
+
+        data = self.embedPoints(data)
         return data
