5a1f3d16e7ab52a4a4db8a77be5d3c9f1d979ce9
chLi <565353780@qq.com>
2022-11-21 19:59:59 +0800

    add trans test

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from points_shape_detect.Test.iou import test as test_iou
+from points_shape_detect.Test.trans import test as test_trans
 
 if __name__ == "__main__":
     test_iou()
+    test_trans()
