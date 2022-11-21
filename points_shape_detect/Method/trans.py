63c9a629960d5a92ec3f6654b5cd2d9f4848cf86
chLi <565353780@qq.com>
2022-11-21 19:58:49 +0800

    finish test translate and rotate, scale still have bugs

--- points_shape_detect/Method/trans.py
+++ points_shape_detect/Method/trans.py
@@ -1,54 +1,100 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import torch
 import numpy as np
-import open3d as o3d
-from scipy.spatial.transform import Rotation as R
 
-from points_shape_detect.Method.matrix import make_M_from_tqs, decompose_mat4
+from points_shape_detect.Method.matrix import getRotateMatrix
+
+
+def getBatchResult(func, batch_data):
+    batch_result = torch.cat(
+        [func(batch_data[i]).unsqueeze(0) for i in range(batch_data.shape[0])])
+    return batch_result
+
+
+def normalizePointArrayTensor(point_array_tensor):
+    min_point_tensor = torch.min(point_array_tensor, 0)[0]
+    max_point_tensor = torch.max(point_array_tensor, 0)[0]
+    min_max_point_tensor = torch.cat([min_point_tensor,
+                                      max_point_tensor]).reshape(2, 3)
+    center = torch.mean(min_max_point_tensor, axis=0)
+
+    origin_point_array_tensor = point_array_tensor - center
+
+    max_bbox_length = torch.max(max_point_tensor - min_point_tensor)
+    normalize_point_array_tensor = origin_point_array_tensor / max_bbox_length
+    return normalize_point_array_tensor
 
 
 def normalizePointArray(point_array):
-    #  centroid = np.mean(point_array, axis=0)
+    if isinstance(point_array, torch.Tensor):
+        assert 2 <= len(point_array.shape) <= 3
+        if len(point_array.shape) == 2:
+            return normalizePointArrayTensor(point_array)
+        else:
+            return getBatchResult(normalizePointArrayTensor, point_array)
+
     min_point = np.min(point_array, axis=0)
     max_point = np.max(point_array, axis=0)
-    centroid = np.mean([min_point, max_point], axis=0)
+    center = np.mean([min_point, max_point], axis=0)
 
-    point_array = point_array - centroid
+    origin_point_array = point_array - center
 
-    #  m = np.max(np.sqrt(np.sum(point_array**2, axis=1)))
-    #  point_array = point_array / m
     max_bbox_length = np.max(max_point - min_point)
-    point_array = point_array / max_bbox_length
-    return point_array
+    normalize_point_array = origin_point_array / max_bbox_length
+    return normalize_point_array
 
 
-def getQuatFromEulerAngle(euler_angle):
-    r = R.from_euler('zxy', euler_angle, degrees=True)
-    return r.as_quat()
+def getInverseTrans(translate, euler_angle, scale):
+    translate_inv = -1.0 * translate
+    euler_angle_inv = -1.0 * euler_angle
+    scale_inv = 1.0 / scale
+    return translate_inv, euler_angle_inv, scale_inv
 
 
-def getRotateMatrixFromEulerAngle(euler_angle):
-    # yaw, roll, pitch
-    r = R.from_euler('zxy', euler_angle, degrees=True)
-    return r.as_matrix()
+def transPointArrayTensor(point_array_tensor,
+                          translate,
+                          euler_angle,
+                          scale,
+                          is_inverse=False):
+    min_point_tensor = torch.min(point_array_tensor, 0)[0]
+    max_point_tensor = torch.max(point_array_tensor, 0)[0]
+    min_max_point_tensor = torch.cat([min_point_tensor,
+                                      max_point_tensor]).reshape(2, 3)
+    center = torch.mean(min_max_point_tensor, axis=0)
+
+    origin_point_array_tensor = point_array_tensor - center
+
+    rotate_matrix = getRotateMatrix(euler_angle, is_inverse)
+
+    origin_trans_point_array_tensor = torch.matmul(origin_point_array_tensor,
+                                                   rotate_matrix)
+
+    trans_point_array = origin_trans_point_array + center + translate
+    return trans_point_array
 
 
-def getInverseTrans(translate, quat, scale):
-    trans_matrix = make_M_from_tqs(translate, quat, scale)
-    trans_matrix_inv = np.linalg.inv(trans_matrix)
-    translate_inv, quat_inv, scale_inv = decompose_mat4(trans_matrix_inv)
-    return translate_inv, quat_inv, scale_inv
+def transPointArray(point_array,
+                    translate,
+                    euler_angle,
+                    scale,
+                    is_inverse=False,
+                    rotate_center=None):
+    if rotate_center is None:
+        center = np.mean(point_array, axis=0)
+    else:
+        center = np.array(rotate_center)
 
+    origin_point_array = point_array - center
 
-def transPointArray(point_array, translate, quat, scale):
-    trans_matrix = make_M_from_tqs(translate, quat, scale)
+    rotate_matrix = getRotateMatrix(euler_angle, is_inverse)
 
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(point_array)
-    pcd.transform(trans_matrix)
+    origin_trans_point_array = origin_point_array @ rotate_matrix
 
-    trans_point_array = np.array(pcd.points)
+    scale_origin_trans_point_array = origin_trans_point_array * scale
+
+    trans_point_array = scale_origin_trans_point_array + center + translate
     return trans_point_array
 
 
@@ -57,20 +103,18 @@ def randomTransPointArray(point_array, need_trans=False):
     euler_angle = (np.random.rand(3) - 0.5) * 360.0
     scale = np.random.rand(3) + 0.5
 
-    quat = getQuatFromEulerAngle(euler_angle)
-    trans_point_array = transPointArray(point_array, translate, quat, scale)
+    trans_point_array = transPointArray(point_array, translate, euler_angle,
+                                        scale)
     if need_trans:
-        return trans_point_array, translate, quat, scale
+        return trans_point_array, translate, euler_angle, scale
     return trans_point_array
 
 
 def moveToOrigin(point_array):
-    min_xyz = np.array([np.min(point_array[:, i]) for i in range(3)])
-    max_xyz = np.array([np.max(point_array[:, i]) for i in range(3)])
-
-    mean_xyz = (min_xyz + max_xyz) / 2.0
-
-    return point_array - mean_xyz
+    min_point = np.min(point_array, axis=0)
+    max_point = np.max(point_array, axis=0)
+    center = np.mean([min_point, max_point], axis=0)
+    return point_array - center
 
 
 def moveToMeanPoint(point_array):
