bd848590879d78f4dd46a4bfd68e413c133f8e97
chLi <565353780@qq.com>
2022-11-21 22:21:05 +0800

    finish basic predict rotate, scale, shape and bbox model

--- points_shape_detect/Model/points_shape_net.py
+++ points_shape_detect/Model/points_shape_net.py
@@ -8,6 +8,7 @@ from torch import nn
 from points_shape_detect.Lib.chamfer_dist import ChamferDistanceL1
 from points_shape_detect.Loss.ious import IoULoss
 from points_shape_detect.Method.sample import fps
+from points_shape_detect.Method.trans import transPointArray, getInverseTrans
 from points_shape_detect.Model.fold import Fold
 from points_shape_detect.Model.pc_transformer import PCTransformer
 
@@ -66,48 +67,82 @@ class PointsShapeNet(nn.Module):
         self.l1_loss = nn.SmoothL1Loss()
         return
 
-    def encodePoints(self, data):
+    def encodeTransPoints(self, data):
         # Bx#pointx3
-        query_point_array = data['inputs']['query_point_array']
+        trans_query_point_array = data['inputs']['trans_query_point_array']
 
         # Bx#pointx3 -[feature_encoder]-> BxMxC and BxMx3
-        encode_feature, coarse_point_cloud = self.feature_encoder(
-            query_point_array)
+        trans_encode_feature, trans_coarse_point_cloud = self.feature_encoder(
+            trans_query_point_array)
 
-        data['predictions']['encode_feature'] = encode_feature
-        data['predictions']['coarse_point_cloud'] = coarse_point_cloud
+        data['predictions']['trans_encode_feature'] = trans_encode_feature
+        data['predictions'][
+            'trans_coarse_point_cloud'] = trans_coarse_point_cloud
         return data
 
-    def decodePointsFeature(self, data):
-        # BxMxC
-        encode_feature = data['predictions']['encode_feature']
-        # BxMx3
-        coarse_point_cloud = data['predictions']['coarse_point_cloud']
+    def lossTransComplete(self, data):
+        loss_trans_coarse = self.loss_func(
+            data['predictions']['trans_coarse_points'],
+            data['inputs']['trans_point_array'])
 
-        B, M, C = data['predictions']['encode_feature'].shape
+        data['losses']['loss_trans_coarse'] = loss_trans_coarse * 1000
+        return data
+
+    def embedTransPoints(self, data):
+        # Bx#pointx3
+        trans_query_point_array = data['inputs']['trans_query_point_array']
+        # BxMx3
+        trans_coarse_point_cloud = data['predictions'][
+            'trans_coarse_point_cloud']
+
+        # Bx#pointx3 -[fps]-> BxMx3
+        trans_fps_query_point_array = fps(trans_query_point_array,
+                                          self.num_query)
+
+        # BxMx3 + BxMx3 -[cat]-> Bx2Mx3
+        trans_coarse_points = torch.cat(
+            [trans_coarse_point_cloud, trans_fps_query_point_array],
+            dim=1).contiguous()
+
+        data['predictions']['trans_coarse_points'] = trans_coarse_points
+
+        if self.training:
+            data = self.lossTransComplete(data)
+        return data
+
+    def decodeTransPointsFeature(self, data):
+        # BxMxC
+        trans_encode_feature = data['predictions']['trans_encode_feature']
+        # BxMx3
+        trans_coarse_point_cloud = data['predictions'][
+            'trans_coarse_point_cloud']
+
+        B, M, C = data['predictions']['trans_encode_feature'].shape
 
         # BxMxC -[transpose]-> BxCxM -[increase_dim]-> Bx1024xM -[transpose]-> BxMx1024
-        points_feature = self.increase_dim(encode_feature.transpose(
-            1, 2)).transpose(1, 2)
+        trans_points_feature = self.increase_dim(
+            trans_encode_feature.transpose(1, 2)).transpose(1, 2)
 
         # BxMx1024 -[max]-> Bx1024
-        global_points_feature = torch.max(points_feature, dim=1)[0]
+        trans_global_points_feature = torch.max(trans_points_feature, dim=1)[0]
 
         # Bx1024 -[unsqueeze]-> Bx1x1024 -[expand]-> BxMx1024
-        replicate_global_points_feature = global_points_feature.unsqueeze(
+        trans_replicate_global_points_feature = trans_global_points_feature.unsqueeze(
             -2).expand(-1, M, -1)
 
         # BxMx1024 + BxMxC + BxMx3 -[cat]-> BxMx(C+1027)
-        global_feature = torch.cat([
-            replicate_global_points_feature, encode_feature, coarse_point_cloud
+        trans_global_feature = torch.cat([
+            trans_replicate_global_points_feature, trans_encode_feature,
+            trans_coarse_point_cloud
         ],
-                                   dim=-1)
+                                         dim=-1)
 
         # BxMx(C+1027) -[reshape]-> BMx(C+1027) -[reduce_map]-> BMxC
-        reduce_global_feature = self.reduce_map(
-            global_feature.reshape(B * M, -1))
+        trans_reduce_global_feature = self.reduce_map(
+            trans_global_feature.reshape(B * M, -1))
 
-        data['predictions']['reduce_global_feature'] = reduce_global_feature
+        data['predictions'][
+            'trans_reduce_global_feature'] = trans_reduce_global_feature
         return data
 
     def lossTrans(self, data):
@@ -127,19 +162,21 @@ class PointsShapeNet(nn.Module):
 
     def encodeTrans(self, data):
         # BMxC
-        reduce_global_feature = data['predictions']['reduce_global_feature']
+        trans_reduce_global_feature = data['predictions'][
+            'trans_reduce_global_feature']
 
-        B, M, C = data['predictions']['encode_feature'].shape
+        B, M, C = data['predictions']['trans_encode_feature'].shape
 
         # BMxC -[reshape]-> BxMCx1 -[bbox_feature_decoder]-> BxCx1
-        bbox_feature = self.bbox_feature_decoder(
-            reduce_global_feature.reshape(B, -1, 1))
+        trans_bbox_feature = self.bbox_feature_decoder(
+            trans_reduce_global_feature.reshape(B, -1, 1))
 
         # BxCx1 -[euler_angle_decoder]-> Bx3x1 -[reshape]-> Bx3
-        euler_angle_inv = self.euler_angle_decoder(bbox_feature).reshape(B, -1)
+        euler_angle_inv = self.euler_angle_decoder(trans_bbox_feature).reshape(
+            B, -1)
 
         # BxCx1 -[scale_decoder]-> Bx3x1 -[reshape]-> Bx3
-        scale_inv = self.scale_decoder(bbox_feature).reshape(B, -1)
+        scale_inv = self.scale_decoder(trans_bbox_feature).reshape(B, -1)
 
         data['predictions']['euler_angle_inv'] = euler_angle_inv
         data['predictions']['scale_inv'] = scale_inv
@@ -148,20 +185,36 @@ class PointsShapeNet(nn.Module):
             data = self.lossTrans(data)
         return data
 
-    def transQueryPointArray(self, data):
+    def transBackQueryPoints(self, data):
         # Bx#pointx3
-        query_point_array = data['inputs']['query_point_array']
+        trans_query_point_array = data['inputs']['trans_query_point_array']
         # Bx3
         euler_angle_inv = data['predictions']['euler_angle_inv']
         # Bx3
         scale_inv = data['predictions']['scale_inv']
 
-        trans_query_point_array = torch.mm(query_point_array 
+        device = trans_query_point_array.device
+
+        query_points_list = []
+
+        translate = torch.tensor([0.0, 0.0, 0.0]).to(device)
+        for i in range(trans_query_point_array.shape[0]):
+            trans_query_points = trans_query_point_array[i]
+            euler_angle = euler_angle_inv[i]
+            scale = scale_inv[i]
+
+            query_points = transPointArray(trans_query_points, translate,
+                                           euler_angle, scale)
+            query_points_list.append(query_points.unsqueeze(0))
+
+        query_point_array = torch.cat(query_points_list)
+
+        data['predictions']['query_point_array'] = query_point_array
         return data
 
-    def encodeTransPoints(self, data):
+    def encodePoints(self, data):
         # Bx#pointx3
-        query_point_array = data['inputs']['query_point_array']
+        query_point_array = data['predictions']['query_point_array']
 
         # Bx#pointx3 -[feature_encoder]-> BxMxC and BxMx3
         encode_feature, coarse_point_cloud = self.feature_encoder(
@@ -277,7 +330,7 @@ class PointsShapeNet(nn.Module):
 
     def embedPoints(self, data):
         # Bx#pointx3
-        query_point_array = data['inputs']['query_point_array']
+        query_point_array = data['predictions']['query_point_array']
         # BxMx3
         coarse_point_cloud = data['predictions']['coarse_point_cloud']
         # BxMSx3
@@ -302,6 +355,16 @@ class PointsShapeNet(nn.Module):
         return data
 
     def forward(self, data):
+        data = self.encodeTransPoints(data)
+
+        data = self.embedTransPoints(data)
+
+        data = self.decodeTransPointsFeature(data)
+
+        data = self.encodeTrans(data)
+
+        data = self.transBackQueryPoints(data)
+
         data = self.encodePoints(data)
 
         data = self.decodePointsFeature(data)
