dfc744ce3b9e976d1e37d49523a48b21a09439ee
chLi <565353780@qq.com>
2022-11-24 15:14:55 +0800

    [DEBUG] test rotate_back

--- points_shape_detect/Demo/trainer.py
+++ points_shape_detect/Demo/trainer.py
@@ -10,11 +10,13 @@ from points_shape_detect.Module.trainer import Trainer
 
 def demo():
     model_file_path = "./output/20221122_23:38:42/model_best.pth"
-    #  model_file_path = ""
+    model_file_path = "./output/20221124_13:47:06/model_best.pth"
+    model_file_path = ""
+    resume_model_only = True
     print_progress = True
 
     trainer = Trainer()
-    trainer.loadModel(model_file_path)
-    #  trainer.testTrain()
-    trainer.train(print_progress)
+    trainer.loadModel(model_file_path, resume_model_only)
+    trainer.testTrain()
+    #  trainer.train(print_progress)
     return True
