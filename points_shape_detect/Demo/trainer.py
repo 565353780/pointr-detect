fc0921d00e19eaef83a7c7733519ae0cc5fce5d7
chLi <565353780@qq.com>
2022-11-24 16:43:28 +0800

    add resume_model_only option

--- points_shape_detect/Demo/trainer.py
+++ points_shape_detect/Demo/trainer.py
@@ -12,11 +12,11 @@ def demo():
     model_file_path = "./output/20221122_23:38:42/model_best.pth"
     model_file_path = "./output/20221124_13:47:06/model_best.pth"
     model_file_path = ""
-    resume_model_only = True
+    resume_model_only = False
     print_progress = True
 
     trainer = Trainer()
     trainer.loadModel(model_file_path, resume_model_only)
-    trainer.testTrain()
-    #  trainer.train(print_progress)
+    #  trainer.testTrain()
+    trainer.train(print_progress)
     return True
