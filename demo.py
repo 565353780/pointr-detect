f72c7f6ac99d4dee7efdc9d893cb075507859c60
chLi <565353780@qq.com>
2022-11-25 23:51:03 +0800

    add rotate_train func

--- demo.py
+++ demo.py
@@ -5,8 +5,10 @@ from points_shape_detect.Demo.detector import \
     demo as demo_detect, \
     demo_mesh as demo_detect_partial_mesh
 from points_shape_detect.Demo.trainer import demo as demo_train
+from points_shape_detect.Demo.rotate_trainer import demo as demo_train_rotate
 
 if __name__ == "__main__":
     #  demo_detect()
     #  demo_detect_partial_mesh()
-    demo_train()
+    #  demo_train()
+    demo_train_rotate()
