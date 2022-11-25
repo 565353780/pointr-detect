e7f69dca56765f045f60fd15740b2de66e235c65
chLi <565353780@qq.com>
2022-11-25 17:06:41 +0800

    fix test import bug

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
+sys.path.append("../udf-generate/")
+
 from points_shape_detect.Test.iou import test as test_iou
 from points_shape_detect.Test.trans import test as test_trans
 
