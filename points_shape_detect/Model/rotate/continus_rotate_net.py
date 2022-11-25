240fc18ac8906daa28739215a57248b116e27c99
chLi <565353780@qq.com>
2022-11-26 00:36:20 +0800

    move rotate_back into trainer, remove gt input, try to predict dataset's forward direction

--- points_shape_detect/Model/rotate/continus_rotate_net.py
+++ points_shape_detect/Model/rotate/continus_rotate_net.py
@@ -24,49 +24,30 @@ class ContinusRotateNet(nn.Module):
             nn.AdaptiveMaxPool1d(output_size=1))
 
         # bx2048 -> bx6
-        self.mlp = nn.Sequential(nn.Linear(2048, 512), nn.LeakyReLU(),
+        self.mlp = nn.Sequential(nn.Linear(1024, 512), nn.LeakyReLU(),
                                  nn.Linear(512, 6))
 
         self.mse_loss = nn.MSELoss()
         return
 
-    @torch.no_grad()
-    def rotateBackPoints(self, data):
-        origin_point_array = data['inputs']['origin_point_array']
-        # Bx#pointx3
-        origin_query_point_array = data['inputs']['origin_query_point_array']
-        rotate_matrix = data['inputs']['rotate_matrix']
-
-        rotate_matrix_inv = rotate_matrix.transpose(1, 2)
-
-        rotate_back_point_array = torch.matmul(origin_point_array,
-                                               rotate_matrix_inv).detach()
-        rotate_back_query_point_array = torch.matmul(
-            origin_query_point_array, rotate_matrix_inv).detach()
-
-        data['inputs']['rotate_back_point_array'] = rotate_back_point_array
-        data['inputs'][
-            'rotate_back_query_point_array'] = rotate_back_query_point_array
-        return data
-
     def encodeRotateMatrix(self, data):
         # B*N*3
-        pt1 = data['inputs']['rotate_back_point_array']
+        #  pt1 = data['inputs']['rotate_back_query_point_array']
         # B*N*3
-        pt2 = data['inputs']['origin_point_array']
+        pt2 = data['inputs']['origin_query_point_array']
 
-        B, N, _ = pt1.shape
+        B, N, _ = pt2.shape
 
         # Bx1024
-        feature_pt1 = self.feature_extracter(pt1.transpose(1, 2)).view(B, -1)
+        #  feature_pt1 = self.feature_extracter(pt1.transpose(1, 2)).view(B, -1)
         # Bx1024
         feature_pt2 = self.feature_extracter(pt2.transpose(1, 2)).view(B, -1)
 
         # Bx2048
-        f = torch.cat((feature_pt1, feature_pt2), 1)
+        #  f = torch.cat((feature_pt1, feature_pt2), 1)
 
         # Bx6
-        rotation = self.mlp(f)
+        rotation = self.mlp(feature_pt2)
 
         # Bx3x3
         rotate_matrix = compute_rotation_matrix_from_ortho6d(rotation)
@@ -76,6 +57,9 @@ class ContinusRotateNet(nn.Module):
 
         #  if self.training:
         data = self.lossRotate(data)
+
+        #  if self.training:
+        data = self.encodeCompleteRotateMatrix(data)
         return data
 
     def lossRotate(self, data):
@@ -89,6 +73,49 @@ class ContinusRotateNet(nn.Module):
 
         data['losses']['loss_rotate_matrix'] = loss_rotate_matrix
         #  data['losses']['loss_geodesic'] = loss_geodesic
+        return data
+
+    def encodeCompleteRotateMatrix(self, data):
+        # B*N*3
+        #  pt1 = data['inputs']['rotate_back_point_array']
+        # B*N*3
+        pt2 = data['inputs']['origin_point_array']
+
+        B, N, _ = pt2.shape
+
+        # Bx1024
+        #  feature_pt1 = self.feature_extracter(pt1.transpose(1, 2)).view(B, -1)
+        # Bx1024
+        feature_pt2 = self.feature_extracter(pt2.transpose(1, 2)).view(B, -1)
+
+        # Bx2048
+        #  f = torch.cat((feature_pt1, feature_pt2), 1)
+
+        # Bx6
+        rotation = self.mlp(feature_pt2)
+
+        # Bx3x3
+        rotate_matrix = compute_rotation_matrix_from_ortho6d(rotation)
+
+        data['predictions']['complete_rotation'] = rotation
+        data['predictions']['complete_rotate_matrix'] = rotate_matrix
+
+        #  if self.training:
+        data = self.lossCompleteRotate(data)
+        return data
+
+    def lossCompleteRotate(self, data):
+        rotate_matrix = data['predictions']['complete_rotate_matrix']
+        gt_rotate_matrix = data['inputs']['rotate_matrix']
+
+        loss_complete_rotate_matrix = torch.pow(
+            gt_rotate_matrix - rotate_matrix, 2).mean()
+        #  loss_complete_geodesic = compute_geodesic_distance_from_two_matrices(
+        #  gt_rotate_matrix, rotate_matrix).mean()
+
+        data['losses'][
+            'loss_complete_rotate_matrix'] = loss_complete_rotate_matrix
+        #  data['losses']['loss_complete_geodesic'] = loss_complete_geodesic
         return data
 
     @torch.no_grad()
@@ -118,11 +145,12 @@ class ContinusRotateNet(nn.Module):
 
         data = setWeight(data, 'loss_rotate_matrix', 1)
         #  data = setWeight(data, 'loss_geodesic', 1)
+
+        data = setWeight(data, 'loss_complete_rotate_matrix', 1)
+        #  data = setWeight(data, 'loss_complete_geodesic', 1)
         return data
 
     def forward(self, data):
-        data = self.rotateBackPoints(data)
-
         data = self.encodeRotateMatrix(data)
 
         #  data = self.rotateBackByPredict(data)
