1be8846cc1acb31cc68ce5321b6cbccc99574058
chLi <565353780@qq.com>
2022-11-27 19:36:07 +0800

    add rotate back test func, test passed

--- points_shape_detect/Model/points_shape_net.py
+++ points_shape_detect/Model/points_shape_net.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import torch
 from torch import nn
 
 from points_shape_detect.Model.encode.points_encoder import PointsEncoder
@@ -17,8 +18,31 @@ class PointsShapeNet(nn.Module):
         self.shape_complete_net = ShapeCompleteNet()
         return
 
+    @torch.no_grad()
+    def rotateBackByPredict(self, data):
+        pt1 = data['inputs']['origin_point_array']
+        pt2 = data['inputs']['origin_query_point_array']
+        rotate_matrix = data['inputs']['rotate_matrix']
+
+        rotate_matrix_inv = rotate_matrix.transpose(1, 2)
+
+        B, N, _ = pt2.shape
+
+        rotate_back_point_array = torch.bmm(pt1, rotate_matrix_inv).detach()
+
+        rotate_back_query_point_array = torch.bmm(pt2,
+                                                  rotate_matrix_inv).detach()
+
+        data['inputs'][
+            'rotate_back_point_array'] = rotate_back_point_array
+        data['inputs'][
+            'rotate_back_query_point_array'] = rotate_back_query_point_array
+        return data
+
     def forward(self, data):
         data = self.points_encoder(data)
         data = self.bbox_net(data)
         data = self.shape_complete_net(data)
+
+        #  data = self.rotateBackByPredict(data)
         return data
